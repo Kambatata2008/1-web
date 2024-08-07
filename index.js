ab96d6639398b52b94247a29ca00cfca45f6809f
Kambatata2008 <vouldzhev.k@gmail.com>
2024-08-07 17:19:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// bebe calc program code
+//hahah
+
+const display = document.getElementById("display");
+
+function appendToDisplay (input) {
+    display.value += input;
+}
+
+function clearDisplay () {
+    display.value = " ";
+}
+
+function calculate () {
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
